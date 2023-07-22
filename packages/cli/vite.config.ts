7c21b2dcfc9aac05845b08e22f822914af37951c
AlexAegis <alexaegis@gmail.com>
2023-07-22 17:05:49 +0200

    chore: update packages

--- packages/cli/vite.config.ts
+++ packages/cli/vite.config.ts
@@ -3,5 +3,9 @@ import { defineLibConfig } from '@alexaegis/vite';
 import { pakk } from 'vite-plugin-pakk';
 
 export default defineLibConfig({
-	plugins: [pakk()],
+	plugins: [
+		pakk({
+			developmentPackageJsonExportsTarget: 'source',
+		}),
+	],
 });
